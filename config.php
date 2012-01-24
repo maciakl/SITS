a6ab532236466eafe9e1971e08f598938fb8b585
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2012-01-24 01:10:13 -0500

    Added the config file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This is the main config file where you define global options.
+ * 
+ * Please change these to your database details. If you want to
+ * run SITS in private mode, change SITS_PUBLIC_MODE to false.
+ *
+ */
+
+define('SITS_DB_HOSTNAME', 	'localhost'	);
+
+define('SITS_DB_NAME', 		'sits'		);
+
+define('SITS_DB_USER', 		'sits_user'	);
+
+define('SITS_DB_PASSWORD', 	'password'	);
+
+define('SITS_PUBLIC_MODE', 	true		);
+
+?>
