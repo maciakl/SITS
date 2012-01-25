6459d56242d0b1b0c97337effa6d3abc693a9045
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2012-01-25 15:51:24 -0500

    Added tag list to the ticket handler results

--- model/ticket.handler.php
+++ model/ticket.handler.php
@@ -2,12 +2,15 @@
 
 require_once("model/handler.php");
 require_once("model/comment.handler.php");
+require_once("model/tag.handler.php");
 
 /**
  * Hanldes the complex read only query required to generate a list of tickets, tags and comment numbers.
  *
  * Essentially this class makes a big join of the relevant tables and spits results one ASSOC array at a time.
  *
+ * - the start() method initiates db connection
+ * - the done() method closes the db connection
  * - the next() method gives you the next array or false if there are no more
  *
  */
@@ -18,18 +21,37 @@ class TicketHandler extends Handler
 		parent::__construct("sits_ticket");
 	}
 
+	/**
+	 * Returns ASSOC array of the next available row, with additional coments and tags rows.
+	 * 
+	 * @return mixed An ASSOC array or false if no records are foune
+	 */
 	function next()
 	{
 		$result = parent::next();
 
-		// if we have results, count comments for that result
+		// if we have results, count comments & tags for that result
 		if($result)
 		{
-			$com = new CommentHandler("sits_comment");
-		
+			// first let's do gomments
+			$com = new CommentHandler();
 			$count = $com->count($result["ticketid"]);
-
 			$result["comment_count"] = $count;
+			$com->done();
+
+			// now tags
+
+			$com = new TagHandler($result["ticketid"]);
+			$com->start();
+			$tags = array();
+
+			while($t = $com->next())
+				$tags[] = array( 'tagname' => $t["tagname"], 'style' => $t["style"] );
+
+			$result["tags"] = $tags;
+
+
+
 		}
 
 		return $result;
