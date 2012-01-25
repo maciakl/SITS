5c4bc670d343065515fcc64d026f626a6eab9d69
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2012-01-25 01:10:44 -0500

    Added a stub for ticket handler class (see README)

---
+++ model/ticket.handler.php
@@ -0,0 +1,18 @@
+<?pho
+
+/**
+ * Hanldes the complex read only query required to generate a list of tickets, tags and comment numbers.
+ *
+ * Essentially this class makes a big join of the relevant tables and spits results one ASSOC array at a time.
+ *
+ * - the next() method gives you the next array or false if there are no more
+ *
+ */
+class TicketHandler
+{
+
+}
+
+
+
+?>
